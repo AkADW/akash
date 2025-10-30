f98ab987890cf18f73938a95ac11b028c52fcbac
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-10-30 07:37:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -86,9 +86,9 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://files.catbox.moe/epi6c5.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/3mq22b.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/hee6pf.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/hee6pf.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
